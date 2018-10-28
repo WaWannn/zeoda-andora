a6bb170e4567901190676082366102f0d39f3f92
WaWannn <31292741+WaWannn@users.noreply.github.com>
2018-10-28 13:11:20 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,19 +5,7 @@ var bot = new Discord.Client();
 bot.login("NTA2MDQ5NzE4MDk1MzE0OTQ0.DrcnZg.LI-WiAaI8A7-gZrcFNWFSLGGELI");
 
 bot.on('ready', function(){
-    bot.user.setGame("Upload 13:09");
+    bot.user.setGame("Upload 13:11");
     console.log("Le bot a bien été connecte");
 });
-
-
-bot.on('message', message => {
-    id ( message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes : \n -*help");
-    }
-    if ( message.content === "salut"){
-        message.reply("Bien le bonjour!!!!");
-        console.log("ok");
-    }
-});
-
 
