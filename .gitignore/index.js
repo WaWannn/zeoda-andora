12630f30eeebcbea8e2a5ef127b5c9cca4fb51c5
WaWannn <31292741+WaWannn@users.noreply.github.com>
2018-10-28 12:31:18 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,8 +4,8 @@ var bot = new Discord.Client();
 
 bot.login("NTA2MDQ5NzE4MDk1MzE0OTQ0.DrcnZg.LI-WiAaI8A7-gZrcFNWFSLGGELI");
 
-bot.on('ready', function(){
-    bot.user.setGame("Upload 12:30");
+bot.on("ready", function(){
+    bot.user.setGame("Upload 12:31");
     console.log("Le bot a bien été connecte");
 });
 
