6354e3f7202a26549f041a6588f75302c482a337
WaWannn <31292741+WaWannn@users.noreply.github.com>
2018-10-28 16:07:19 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,16 @@
 const Discord = require("discord.js");
 var bot = new Discord.Client();
 
-var questArray = new Array();
+// Level and xD
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapters = new FileSync('database.json');
+const db = low(adapters);
+
+
+
+
 
 
 
@@ -10,7 +19,7 @@ var questArray = new Array();
 bot.login("NTA2MDQ5NzE4MDk1MzE0OTQ0.DrcnZg.LI-WiAaI8A7-gZrcFNWFSLGGELI");
 
 bot.on('ready' , function() {
-    bot.user.setGame('15:59');
+    bot.user.setGame('16:07');
 });
 
 // Message de bienvenu
@@ -33,3 +42,45 @@ bot.on('message', function (message) {
         message.reply('Voici votre liste de quêtes :');
     }
 });
+
+
+
+
+
+
+
+
+
+
+ 
+db.defaults({ histoires : [], xp: []}).write()
+ 
+bot.on('message', message => {
+   
+    var msgauthor = message.author.id
+ 
+    if(message.author.bot)return;
+ 
+    if(!db.get("xp").find({user : msgauthor}).value()){
+        db.get("xp").push({user : msgauthor, xp: 1}).write();
+    }else{
+        var userxpdb = db.get("xp").filter({user : msgauthor}).find("xp").value();
+        console.log(userxpdb)
+        var userxp = Object.values(userxpdb)
+        console.log(userxp)
+        console.log(`Nombre d'xp: ${userxp[1]}`)
+ 
+        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
+ 
+        if(message.content === prefix + "xp"){
+            var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+            var xpfinal = Object.values(xp);
+            var xp_embed = new Discord.RichEmbed()
+                .setTitle(`Stat des XP de : ${message.author.username}`)
+                .setColor('#F4D03F')
+                .addField("XP", `${xpfinal[1]} xp`)
+                .setFooter("Enjoy :p")
+            message.channel.send({embed : xp_embed})
+        }
+    }
+})
