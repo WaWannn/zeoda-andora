61c5e9605b4d7937e9aa0dc5ee8c03f6a23558d2
WaWannn <31292741+WaWannn@users.noreply.github.com>
2018-10-28 15:31:42 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,16 +1,19 @@
 const Discord = require("discord.js");
 var bot = new Discord.Client();
 
+var level = new Array();
 
 bot.login("NTA2MDQ5NzE4MDk1MzE0OTQ0.DrcnZg.LI-WiAaI8A7-gZrcFNWFSLGGELI");
 
 bot.on('ready' , function() {
-    bot.user.setGame('15:22');
+    bot.user.setGame('15:31');
 });
 
 // Message de bienvenu
 bot.on( 'guildMemberAdd' , function( member) {
     member.guild.channels.find('name', 'accueil').send('Bienvenue dans le culte de Zeoda ' + member.user + '. Andora vous a envoyé votre première quete. Pour consulter les quêtes, utilisez la commande !quete')
+
+
 });
 
 
@@ -18,8 +21,12 @@ bot.on( 'guildMemberAdd' , function( member) {
 // Liste des quêtes
 bot.on('message', function (message) {
     if ( message.content === "!quete") {
-        message.reply( message.author.username + ' Voici votre liste de quêtes 3:');
+        message.send( message.author.username + ' Voici votre liste de quêtes :');
     }
 });
+
+
+
+
 
 
