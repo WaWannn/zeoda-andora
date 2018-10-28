4452cb219afabce61c6b8507023d7b8a31ab92b3
WaWannn <31292741+WaWannn@users.noreply.github.com>
2018-10-28 13:30:53 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,13 +4,13 @@ var bot = new Discord.Client();
 
 bot.login("NTA2MDQ5NzE4MDk1MzE0OTQ0.DrcnZg.LI-WiAaI8A7-gZrcFNWFSLGGELI");
 
+bot.on('ready', function(){
 
-bot.on("message", message => {
-    id ( message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes : \n -*help");
-    }
-    if ( message.content === "salut"){
-        message.reply("Bien le bonjour!!!!");
-        console.log("ok");
+});
+
+
+bot.on('message', function (message) {
+    if ( message.content === "!ping") {
+        message.channel.send("pong");
     }
 });
