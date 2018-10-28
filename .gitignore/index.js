87b4569d4e7318dfff49169058188045b7461a69
WaWannn <31292741+WaWannn@users.noreply.github.com>
2018-10-28 14:07:27 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,12 +5,20 @@ var bot = new Discord.Client();
 bot.login("NTA2MDQ5NzE4MDk1MzE0OTQ0.DrcnZg.LI-WiAaI8A7-gZrcFNWFSLGGELI");
 
 bot.on('ready' , function() {
-    bot.user.setGame("13:59");
-}
+    bot.user.setGame('14:07');
+});
+
+// Message de bienvenu
+bot.on( 'guildMemberAdd' , function( member) {
+   member.createDM().then(function(channel) {
+       channel.send('Bienvenue dans le culte de Zeoda ' + member.displayName);
+   });
+});
+
 
 // Liste des quêtes
 bot.on('message', function (message) {
     if ( message.content === "!quete") {
-        message.reply( message.author.username + " Voici votre liste de quêtes 3:");
+        message.reply( message.author.username + ' Voici votre liste de quêtes 3:');
     }
 });
